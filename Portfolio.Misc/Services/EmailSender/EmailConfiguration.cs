85c534536e67d0be7478a75acf83912507ff02e4
Amir07-kz <75159735+Amir07-kz@users.noreply.github.com>
2022-03-07 00:00:03 +0300

    Site
    Site

---
+++ Portfolio.Misc/Services/EmailSender/EmailConfiguration.cs
@@ -0,0 +1,11 @@
+namespace Portfolio.Misc.Services.EmailSender
+{
+    public class EmailConfiguration
+    {
+        public int Port { get; set; }
+        public string From { get; set; }
+        public string SmtpServer { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Portfolio/Controllers/ContactsController.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Misc.Services.EmailSender;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+
+    public class ContactsController : Controller
+    {
+        private IEmailService EmailSender;
+
+        public ContactsController(IEmailService _emailSender)
+        {
+            EmailSender = _emailSender;
+        }
+
+        [HttpGet]
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contacts([FromForm] User userData)
+        {
+
+            var message = new Message(new string[] { "testaccnum00001@gmail.com" }, "Users Data", $"Email:{userData.Email}\nName:{userData.Name}\nSubject:{userData.Subject}\nMessage:\n{userData.Message}");
+            await EmailSender.SendEmailAsync(message);
+            return Ok();
+        }
+    }
+
+}
